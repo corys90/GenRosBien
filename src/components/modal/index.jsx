8e40e8016c5815eeba2bc9298b554c879221c71d
Cristian A. Cortes Sarmiento <corys90@hotmail.com>
2023-01-08 19:20:34 -0500

    Update: eliminación de warnings en modal.index

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import {Button} from 'react-bootstrap';
+import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import logo from '../modal/Vector.png';
 import './style.css';
