4fb727671e269af9151507638e2c4af0c2f42504
Cristian A. Cortes Sarmiento <corys90@hotmail.com>
2023-01-28 16:42:06 -0500

    Update: Login Mobile Ok

--- src/pages/PageLogin/FrmLogin/index.tsx
+++ src/pages/PageLogin/FrmLogin/index.tsx
@@ -4,20 +4,27 @@ import './style.css';
 
 const logo = require('./relaxed-woman-enjoying-sea.png');
 
+const bordeTextBox = {
+  width: "100%", 
+  border: "0px",
+  borderStyle: "solid",
+  borderRadius: "0px",
+  borderBottomWidth: "2px",
+  borderColor: "#999999",
+  backgroundColor: "red"
+}
+
 function FrmLogin() {
 
   const navigate = useNavigate();
 
   const validar = () => {
-
-    alert("Ingresar...!!!");
     navigate("/home");
-}
+  }
 
-const recoveryPassword = () => {
-  alert("Recuperar...!!!");
-  navigate("/passwordRecovery");
-}
+  const recoveryPassword = () => {
+    navigate("/passwordRecovery");
+  }
 
   return (
     <div className='containerLogin'>
@@ -28,19 +35,21 @@ const recoveryPassword = () => {
             </div>
             <form>
                 <div className=" mb-3 divUserPwd">
-                  <input type="text" className="form-control" 
-                    style={{width: 341}} id="txtUser" placeholder="Nombre de usuario"/>
+                  <input type="text" className="form-control " 
+                    style={bordeTextBox} 
+                    id="txtUser" placeholder="Nombre de usuario"/>
                 </div>
                 <div style={{height: "20px"}}></div>
                 <div className=" mb-3 divUserPwd ">
                   <input type="password" className="form-control" 
-                  style={{width: 341}} id="txtPassword" placeholder="Contraseña"/>
+                  style={bordeTextBox}  id="txtPassword" placeholder="Contraseña"/>
                 </div>              
             </form>
             <div style={{height: "20px"}}></div>
             <div className="mb-3 divOlvidoPwd">
               <nav>
-                <Link to="/passwordRecovery" className='text-decoration-none' onClick={recoveryPassword}>¿Haz olvidado tu contraseña?
+                <Link to="/passwordRecovery" className='text-decoration-none' onClick={recoveryPassword}  style={{fontSize: "14px"}}>
+                  ¿Haz olvidado tu contraseña?
                   <br />
                   clic aquí.
                 </Link> 
@@ -48,7 +57,7 @@ const recoveryPassword = () => {
             </div>
             <div style={{height: "20px"}}></div>
             <div className=" mb-3 text-center ">
-              <Button className="btn btn-danger " onClick={validar}>Ingresar</Button>
+              <Button className="btn btn-danger rounded-0 " onClick={validar} style={{width: "100%", height: "45px"}}>Ingresar</Button>
             </div>
           </div>
           <div className=" img-fluid divImgRelax">
